ac44e5a2ccf80f0a08a21d23d87ba04892d2f8a9
AlexThuander <thuander234@gmail.com>
2020-09-03 01:38:57 +0900

    lavel changed

--- src/Home.js
+++ src/Home.js
@@ -26,24 +26,24 @@ class Home extends Component {
 	render() {
 		return (
 			<div className="container2">
-				<div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
+				{/* <div style={{fontSize: "14px", background: "white", width: "10%", textAlign: "center", margin: "auto", marginBottom: "10px"}}>
 					Source code: 
 					<IconButton style={{color: "black"}} onClick={() => window.location.href="https://github.com/0x5eba/Video-Meeting"}>
 						<GitHubIcon />
 					</IconButton>
-				</div>
+				</div> */}
 				
-				<div>
-					<h1 style={{ fontSize: "45px" }}>Video Meeting</h1>
-					<p style={{ fontWeight: "200" }}>Video conference website that lets you stay in touch with all your friends.</p>
+				<div style={{ marginTop: "64px" }}>
+					<h1 style={{ fontSize: "45px" }}>Screen Sharing</h1>
+					<p style={{ fontWeight: "200" }}>Your screen will be shared on https://binaryrenko.com/creative-investment-model</p>
 				</div>
 
 				<div style={{
 					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
 					textAlign: "center", margin: "auto", marginTop: "100px"
 				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
+					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start sharing your screen</p>
+					{/* <Input placeholder="URL" onChange={e => this.handleChange(e)} /> */}
 					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
 				</div>
 			</div>
